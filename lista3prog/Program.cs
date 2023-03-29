e37a49b54c00f4f5ee9d00f6fbbe6be8fb00cc28
LuizRicardo777 <l.salles@aluno.ifsp.edu.br>
2023-03-29 17:43:33 -0300

    .

--- lista3prog/Program.cs
+++ lista3prog/Program.cs
@@ -18,6 +18,7 @@ namespace lista3prog
             Console.WriteLine("3- exercicio 3");
             Console.WriteLine("4- exercicio 4");
             Console.WriteLine("5- exercicio 5");
+            Console.WriteLine("6- exercicio 6");
             Console.WriteLine("==============");
             num = int.Parse(Console.ReadLine());
 
@@ -170,10 +171,170 @@ namespace lista3prog
                     }
 
                     break;
-                    //===========================================================================================
+                //===========================================================================================
+
+                case 4:
 
 
+                    double n1, n2,conta,carac;
+                    
+                    Console.WriteLine("digite um numero:");
+                    n1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("digite outro numero:");
+                    n2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um numero para escolher algum caracter(1- *, 2- /, 3-  -, 4- +)");
+                    carac = double.Parse(Console.ReadLine());
 
+                    switch (carac)
+
+                    {
+                        case 1:
+                            conta = n1 * n2;
+                            Console.WriteLine("o resultado das informações inseridas foi:" + conta);
+                            break;
+
+                        case 2:
+                            conta = n1 / n2;
+                            Console.WriteLine("o resultado das informações inseridas foi:" + conta);
+                            break;
+                        case 3:
+                            conta = n1 - n2;
+                            Console.WriteLine("o resultado das informações inseridas foi:" + conta);
+                            break;
+                        case 4:
+                            conta = n1 + n2;
+                            Console.WriteLine("o resultado das informações inseridas foi:" + conta);
+                            break;
+                        
+
+
+                        default:
+                            Console.WriteLine("opção não foi dada");
+                            break;
+                    }
+
+
+                    break;
+                //===================================================================================================
+
+               
+
+                case 5:
+                    double sal, x, aumento, diferenca, xr;
+                    Console.WriteLine("digite o salario: ");
+                    sal = double.Parse(Console.ReadLine());
+                    Console.WriteLine("digite o codigo do cargo do funcionario: ");
+                    x = double.Parse(Console.ReadLine());
+
+                    switch (x)
+                    {
+                        case 101:
+                            aumento = 0.075 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        case 102:
+                            aumento = 0.097 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        case 103:
+                            aumento = 0.117 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        case 204:
+                            aumento = 0.089 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        case 206:
+                            aumento = 0.1324 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        case 301:
+                            aumento = 0.104 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        case 302:
+                            aumento = 0.146 * sal;
+                            xr = aumento + sal;
+
+                            Console.WriteLine("o salario antigo era de " + sal + " com o ajuste ficou " + xr + " a diferença é de " + aumento);
+                            break;
+                        default:
+                            Console.WriteLine("codigo nao encontrado");
+                            break;
+                    }
+
+                            break;
+                //===================================================================================================================
+
+                case 6:
+                    double resultado, tipo, imp, imp2;
+                    Console.WriteLine("Imposto de Renda dos cidadãos brasileiros");
+                    Console.WriteLine("1- Até R$ 1.903,98");
+                    Console.WriteLine("2- Entre R$ 1.903,99 e R$ 2.826,65");
+                    Console.WriteLine("3- Entre R$ 2.826,66 e R$ 3.751,05");
+                    Console.WriteLine("4- Entre R$ 3.751,06 e R$ 4.664,68");
+                    Console.WriteLine("5- Acima de R$ 4.664,68");
+                    Console.WriteLine("escolha a opcao que se encaixa na sua situaçãao: ");
+                    tipo = double.Parse(Console.ReadLine());
+
+                    switch (tipo)
+                    {
+                        case 1:
+                            Console.WriteLine("seu salario sera o mesmo sem impostos");
+                            break;
+                        case 2:
+                            Console.WriteLine("qual o seu salario?");
+                            imp = double.Parse(Console.ReadLine());
+
+                            resultado = imp * 0.075f;
+                            imp2 = imp - resultado;
+
+                            Console.WriteLine("seu salario menos os impostos sera de: " + imp2 + " o valor total de imposto sera de: " + resultado);
+                            break;
+                        case 3:
+                            Console.WriteLine("qual o seu salario?");
+                            imp = double.Parse(Console.ReadLine());
+
+                            resultado = imp * 0.15f;
+                            imp2 = imp - resultado;
+
+                            Console.WriteLine("seu salario menos os impostos sera de: " + imp2 + " o valor total de imposto sera de: " + resultado);
+                            break;
+                        case 4:
+                            Console.WriteLine("qual o seu salario?");
+                            imp = double.Parse(Console.ReadLine());
+
+                            resultado = imp * 0.225f;
+                            imp2 = imp - resultado;
+
+                            Console.WriteLine("seu salario menos os impostos sera de: " + imp2 + " o valor total de imposto sera de: " + resultado);
+                            break;
+                        case 5:
+                            Console.WriteLine("qual o seu salario?");
+                            imp = double.Parse(Console.ReadLine());
+
+                            resultado = imp * 0.275f;
+                            imp2 = imp - resultado;
+
+                            Console.WriteLine("seu salario menos os impostos sera de: " + imp2 + " o valor total de imposto sera de: " + resultado);
+                            break;
+                        default:
+                            Console.WriteLine("opção invalida");
+                            break;
+                    }
+                            break;
 
 
 
